b6603f76a46f38d412fc00411fdf3d3bff23b35c
fyodor-bestoloch <71459527+fyodor-bestoloch@users.noreply.github.com>
2020-12-25 23:02:43 +0600

    Update 3. belittling.cpp

--- 1-семестр/2020.09.24/3. belittling.cpp
+++ 1-семестр/2020.09.24/3. belittling.cpp
@@ -1,36 +1,25 @@
-#include<iostream>
+#include <vector>
+#include <iostream>
 using namespace std;
-int* deleting(int *arr, int &b)
-{
-    int h=0;
-    for (unsigned i=0;i<b;i++)
-    {
-        if (arr[i]%2==0)
-        {
-          int tmp=arr[h];
-          arr[h]=arr[i];
-          arr[i]=tmp;
-          h++;
-        }
-    }
-    b=b-h;
-    for (int i=0;i<b;i++)
-    arr[i]=arr[i+h];
-    return arr;
-    delete [] arr;
-}
-int main()
-{
-  int N;
-  cin>>N;
-  int *arr1=new int[N];
-  for (int i=0;i<N;i++)
-  {
-    arr1[i]=rand()%4232;
-    cout<<arr1[i]<< " ";
-  }
+int main() {
+	int n,f;
+	vector<int> temp;
+	cin>>n;
+	vector<int> A;
+	for (int i=0;i<n;i++) {
+		f=rand()%4534;
+    cout<<f<<" ";
+		A.push_back(f);
+	}
+	for (int i=0;i<n;i++) {
+		if (A[i]%2!=0) temp.push_back(A[i]);
+	}
+	A.clear();
+	A=temp;
   cout<<"\n";
-  arr1=deleting(arr1, N);
-  for (int i=0;i<N;i++) cout<<arr1[i]<<" ";
-  delete [] arr1;
+	for(int i=0;i<A.size();i++) {
+		cout<<A[i]<<" ";
+	}
+	return 0;
 }
+
